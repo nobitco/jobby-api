681ba88b7a1646505e1068f779a4e3cf4c68a636
jesmarquez <jesmqz@gmail.com>
2018-03-23 23:23:11 -0500

    Crea ENDPOINT GET entregas

--- api.js
+++ api.js
@@ -5,10 +5,21 @@ const express = require('express')
 
 const api = express.Router()
 const practicantesFixtures = require('./tests/fixtures/practicantes')
+const usuariosFixtures = require('./tests/fixtures/usuarios')
 
 api.get('/practicantes', (req, res) => {
   debug('get practicantes')
   res.send(JSON.stringify(practicantesFixtures.all))
+})
+
+api.get('/usuario', (req, res) => {
+  debug('GET usuario')
+  res.send(JSON.stringify(usuariosFixtures.single))
+})
+
+api.get('/entregas', (req, res) => {
+  debug('GET entregas')
+  res.send(JSON.stringify(practicantesFixtures.entregas))
 })
 
 module.exports = api
--- tests/fixtures/practicantes.js
+++ tests/fixtures/practicantes.js
@@ -17,6 +17,24 @@ const practicantes = [
   extend(practicante, { id: 4, nombres: faker.name.firstName(), apellidos: faker.name.lastName(), email: faker.internet.email() })
 ]
 
+var fecha1 = new Date(faker.date.future())
+var fecha2 = new Date(faker.date.future())
+var fecha3 = new Date(faker.date.future())
+var fecha4 = new Date(faker.date.future())
+
+const entrega = {
+  id: 1,
+  nombre: 'informe a',
+  fechaEntrega: fecha1.toDateString()
+}
+
+const entregas = [
+  entrega,
+  extend(entrega, { id: 2, nombre: 'actividad b', fechaEntrega: fecha2.toDateString() }),
+  extend(entrega, { id: 3, nombre: 'actividad c', fechaEntrega: fecha3.toDateString() }),
+  extend(entrega, { id: 4, nombre: 'actividad d', fechaEntrega: fecha4.toDateString() })
+]
+
 function extend (obj, values) {
   const clone = Object.assign({}, obj)
 
@@ -25,5 +43,7 @@ function extend (obj, values) {
 
 module.exports = {
   single: practicante,
-  all: practicantes
+  all: practicantes,
+  entrega: entrega,
+  entregas: entregas
 }
