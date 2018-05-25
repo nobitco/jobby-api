80df090d8b1412bb42c98c1fa3601ffeac68faae
jesmarquez <jesmqz@gmail.com>
2018-05-25 17:46:14 -0500

    Create users test

--- tests/api-tests.js
+++ tests/api-tests.js
@@ -2,18 +2,53 @@
 
 const test = require('ava')
 const request = require('supertest')
-const server = require('../server')
-const fixturePracticantes = require('./fixtures/practicantes')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
-test.serial.cb('/api/practicantes', t => {
+const userFixtures = require('./fixtures/users')
+const config = require('../config')
+
+let sandbox = null
+let server = null
+let dbStub = null
+let UserStub = {}
+
+test.beforeEach(async () => {
+  sandbox = sinon.sandbox.create()
+
+  dbStub = sandbox.stub()
+  dbStub.returns(Promise.resolve({
+    User: UserStub,
+  }))
+
+  UserStub.findAll = sandbox.stub()
+  UserStub.findAll.returns(Promise.resolve(userFixtures.all))
+
+  const api = proxyquire('../api', {
+    'jobby-db': dbStub
+  })
+
+  server = proxyquire('../server', {
+    './api': api
+  })
+})
+
+test.afterEach(() => {
+  sandbox && sinon.sandbox.restore()
+})
+
+test.serial.cb('/api/users', t => {
   request(server)
-    .get('/api/practicantes')
+    .get('/api/users')
     .expect(200)
-    .expect('Content-Type', /json/)
     .end((err, res) => {
       t.falsy(err, 'should not return an error')
-      let body = res.body
-      t.deepEqual(body, {}, 'response body should be the expected')
+      let body = JSON.stringify(res.body)
+      let expected = JSON.stringify(userFixtures.all)
+      t.deepEqual(body, expected, 'response body should be the expected')
       t.end()
     })
 })
+
+test.serial.todo('/api/users - not authorized')
+test.serial.todo('/api/user/:username')
